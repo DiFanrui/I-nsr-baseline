f2623c63e82ba413154fd1b259e983ace41e9b85
DiFanrui <2829107824@qq.com>
2025-05-05 21:39:34 +0800

    从1到91提取十张测试，效果不好

---
+++ util_file.sh
@@ -0,0 +1,26 @@
+# sudo rm -rf data/lung_511_for_debug/images/00000001.jpg
+# sudo rm -rf data/lung_511_for_debug/images/00000001.png
+# sudo rm -rf data/lung_511_for_debug/images/00000011.jpg
+# sudo rm -rf data/lung_511_for_debug/images/00000011.png
+# sudo rm -rf data/lung_511_for_debug/images/00000051.jpg
+# sudo rm -rf data/lung_511_for_debug/images/00000051.png
+sudo cp data/lung_511/images/00000001.jpg data/lung_511_for_debug/images/
+sudo cp data/lung_511/images/00000001.png data/lung_511_for_debug/images/
+sudo cp data/lung_511/images/00000011.jpg data/lung_511_for_debug/images/
+sudo cp data/lung_511/images/00000011.png data/lung_511_for_debug/images/
+sudo cp data/lung_511/images/00000021.jpg data/lung_511_for_debug/images/
+sudo cp data/lung_511/images/00000021.png data/lung_511_for_debug/images/
+sudo cp data/lung_511/images/00000031.jpg data/lung_511_for_debug/images/
+sudo cp data/lung_511/images/00000031.png data/lung_511_for_debug/images/
+sudo cp data/lung_511/images/00000041.jpg data/lung_511_for_debug/images/
+sudo cp data/lung_511/images/00000041.png data/lung_511_for_debug/images/
+sudo cp data/lung_511/images/00000051.jpg data/lung_511_for_debug/images/
+sudo cp data/lung_511/images/00000051.png data/lung_511_for_debug/images/
+sudo cp data/lung_511/images/00000061.jpg data/lung_511_for_debug/images/
+sudo cp data/lung_511/images/00000061.png data/lung_511_for_debug/images/
+sudo cp data/lung_511/images/00000071.jpg data/lung_511_for_debug/images/
+sudo cp data/lung_511/images/00000071.png data/lung_511_for_debug/images/
+sudo cp data/lung_511/images/00000081.jpg data/lung_511_for_debug/images/
+sudo cp data/lung_511/images/00000081.png data/lung_511_for_debug/images/
+sudo cp data/lung_511/images/00000091.jpg data/lung_511_for_debug/images/
+sudo cp data/lung_511/images/00000091.png data/lung_511_for_debug/images/
